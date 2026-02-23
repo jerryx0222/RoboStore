5a420110a0f407da2e1e7f74020ad802dc44e21e
jerryx0222 <jerryx0222@gmail.com>
2026-02-23 13:06:19 +0800

    Spring boot project: Robostore

---
+++ robostore/src/main/java/com/robostore/RobostoreApplication.java
@@ -0,0 +1,13 @@
+package com.robostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobostoreApplication.class, args);
+	}
+
+}
---
+++ robostore/src/test/java/com/robostore/RobostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
