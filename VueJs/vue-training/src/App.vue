bbf3403243db9e9591f3472250fcf703bd7e0077
JerryHo <jerryx0222@gmail.com>
2026-02-24 22:48:36 +0800

    update 20260224-4

---
+++ VueJs/vue-training/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+    console.log("我的第一個組件");
+    let data="Hell1";
+    let htmlData="<u>Hello2</u>"
+    let name="content";
+    let bData=true;
+
+</script>
+
+<template>
+    <nav>基本的導列</nav>
+    <main>基本的主內容</main>
+    <div>{{data}}</div>
+    <div v-html="htmlData"></div>
+    <div v-bind:class="name">Hello3</div>
+    <div :class="name">Hello4</div>
+    <div>{{ data.toUpperCase() }}</div>
+    <div :class="bData?'dark':'light'">Hello5</div>
+
+</template>
+
+<style scoped>
+    main{background-color:#dddddd;}
+</style>
