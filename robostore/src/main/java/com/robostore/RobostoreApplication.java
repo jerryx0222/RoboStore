6041865e0212527663d76fc7c5a1e776453e2d5f
jerryx0222 <jerryx0222@gmail.com>
2026-02-24 14:18:14 +0800

    新增index的內容字串

--- robostore/src/main/java/com/robostore/RobostoreApplication.java
+++ robostore/src/main/java/com/robostore/RobostoreApplication.java
@@ -3,11 +3,24 @@ package com.robostore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
 @SpringBootApplication
 public class RobostoreApplication {
 
 	public static void main(String[] args) {
+		// http://127.0.0.1:8080/
 		SpringApplication.run(RobostoreApplication.class, args);
+
 	}
+
+	@GetMapping("/")
+	public String index(){
+		return "Hello World Spring boot!";
+	}
+	
 
 }
